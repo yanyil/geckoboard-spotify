a4efae65ba91c1bc9db5fbe17663fe221a085db4
Yan-Yi Li <liyanyi@gmail.com>
2016-09-29 22:21:19 +0100

    Create dataset
    - add dataset with example data

--- geckoboard.rb
+++ geckoboard.rb
@@ -4,4 +4,16 @@ Dotenv.load
 
 API_KEY = ENV['GECKOBOARD_API_KEY']
 client = Geckoboard.client(API_KEY)
-p client.ping
+client.ping
+
+dataset = client.datasets.find_or_create('artist', fields: [
+  Geckoboard::NumberField.new(:followers, name: 'Followers'),
+  Geckoboard::NumberField.new(:popularity, name: 'Popularity'),
+])
+
+dataset.put([
+  {
+    followers: 1000000,
+    popularity: 50
+  },
+])
